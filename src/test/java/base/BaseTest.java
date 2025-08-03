0851ced703fb459c6fbca4fdbfaefaa231a092b5
SADHIN <30hasanmahedi@gmail.com>
2025-08-04 00:42:52 +0600

    update jenkins

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -39,6 +39,9 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+            driver = null; // Reset for safety in case of future parallel runs
+        }
     }
 }
