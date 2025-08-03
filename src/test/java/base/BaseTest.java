a844eabaf21278d69774eb9c5f1467b6814a4ec0
Md Mahedi hasan <55445546+mookto@users.noreply.github.com>
2025-08-04 01:12:32 +0600

    Update BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.IOException;
+
 
 import java.util.UUID;
 
