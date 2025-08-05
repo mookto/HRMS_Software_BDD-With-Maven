527ed119169c0f4f5e8d7ad5a05c036688fe86f7
SADHIN <30hasanmahedi@gmail.com>
2025-08-05 22:16:00 +0600

    Pom and jenkins file updated

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	    features = "src/test/resources/features",
 	    glue = "stepsDefs",
-	    plugin = {"pretty", "html:target/cucumber-report.html","json:target/cucumber.json"},
+	    plugin = {"pretty", "html:target/cucumber-html-reports", "json:target/cucumber-reports/cucumber.json"},
 	    monochrome = true
 	)
 public class TestRunner extends AbstractTestNGCucumberTests{
